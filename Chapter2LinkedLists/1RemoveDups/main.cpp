82a382f829f713b4cdf28cb5aa84c6017822ec29
DuParkLinux <dupark3@gmail.com>
2018-06-14 10:28:00 -0400

    No temporary buffer solution added. space complexity of O(1) but time complexity of O(n^2) where n is the number of elements in the list.

--- Chapter2LinkedLists/1RemoveDups/main.cpp
+++ Chapter2LinkedLists/1RemoveDups/main.cpp
@@ -24,6 +24,23 @@ void remove_duplicates(list<int>& my_list){
     }
 }
 
+void remove_duplicates_no_buffer(list<int>& my_list){
+    auto slow_it = my_list.begin();
+    
+    while (slow_it != my_list.end()){
+    auto fast_it = slow_it;
+    ++fast_it;
+        while (fast_it != my_list.end()){
+            if (*slow_it == *fast_it){
+                fast_it = my_list.erase(fast_it);
+            } else {
+                ++fast_it;
+            }
+        }
+    ++slow_it;
+    }
+}
+
 int main(){
     list<int> my_list = {1, 3, 9, 7, 1, 5, 7, 7, 2, 4, 10, 1};
     
@@ -33,7 +50,7 @@ int main(){
     }
     cout << endl;
 
-    remove_duplicates(my_list);
+    remove_duplicates_no_buffer(my_list);
 
     cout << "REMOVED DUPS : ";
     for (auto i = my_list.begin(); i != my_list.end(); ++i){
