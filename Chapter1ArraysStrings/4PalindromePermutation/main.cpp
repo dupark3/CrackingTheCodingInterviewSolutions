44ab9b7466ca0fbfdb24d92363480d4369280efe
DuParkLinux <dupark3@gmail.com>
2018-06-14 08:16:52 -0400

    Palindrome Permutation working by creating a hash table of chars and if more than one char is an odd number occurance, it is not a palindrome.

--- Chapter1ArraysStrings/4PalindromePermutation/main.cpp
+++ Chapter1ArraysStrings/4PalindromePermutation/main.cpp
@@ -11,7 +11,26 @@ Given a string, write a function to check if it is a permutation of a plaindrome
 using namespace std;
 
 bool IsPermutationOfPalindrome(const std::string& input){
-    
+    unordered_map<char, int> char_count;
+    size_t input_size = input.size();
+
+    for (size_t i = 0; i != input_size; ++i){
+        ++char_count[input[i]];
+    }
+
+    int odd_chars = 0;
+    for (auto i = char_count.begin(); i != char_count.end(); ++i){
+        if (i->second % 2 == 0){
+            // nothing to do if char occurs even number of times
+        } else if (odd_chars == 1){
+            // if more than one char occurs an odd number of times, not a palindrome
+            return false;
+        } else {
+            // first odd char is allowed since it can go in the middle
+            ++odd_chars;
+        }
+    }
+    return true;
 }
 
 int main(){
@@ -21,9 +40,9 @@ int main(){
 
 
     if (IsPermutationOfPalindrome(input)){
-        cout << "TRUE";
+        cout << "TRUE\n";
     } else {
-        cout << "FALSE";
+        cout << "FALSE\n";
     }
 
     return 0;
