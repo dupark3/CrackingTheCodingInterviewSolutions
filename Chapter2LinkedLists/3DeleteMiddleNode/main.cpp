10ad08a35d777379ddb1840ce290316c10b8c772
DuParkLinux <dupark3@gmail.com>
2018-06-15 08:18:03 -0400

    Delete middle template function implemented. Get a temporary pointer to point to the head, then advance until it precedes the middle node. Connect temp with middle's next node and then delete node.

--- Chapter2LinkedLists/3DeleteMiddleNode/main.cpp
+++ Chapter2LinkedLists/3DeleteMiddleNode/main.cpp
@@ -10,6 +10,8 @@ given only acces to that node.
 
 using namespace std;
 
+/************** SINGLY LINKED LIST IMPLEMENTATION ******************/
+
 // forward declaration
 template <class T> class node;
 
@@ -46,17 +48,43 @@ class node {
         node<T>* next;
 };
 
+
+/***************** SOLUTION TO THE PROBLEM ***********************/
+
+template <class T>
+node<T>* delete_middle_node(List<T>& linked_list, node<T>*& middle_node){
+    node<T>* temp = linked_list.begin();
+    
+    // advance a pointer to the node preceding the middle node
+    while (temp->next != middle_node){
+        temp = temp->next;
+    }
+
+    temp->next = middle_node->next;
+    delete middle_node;
+    return temp->next;
+}
+
+
+
 int main(){
     List<int> my_list;
+
     my_list.append(5);
     my_list.append(7);
-
     node<int>* middle_node = my_list.append(10);
-
     my_list.append(13);
     my_list.append(15);
 
     cout << "ORIGINAL LIST: ";
+    for (auto i = my_list.begin(); i != 0; i = i->next){
+        cout << i->element << ' ';
+    }
+    cout << endl;
+
+    delete_middle_node(my_list, middle_node);
+
+    cout << "MIDDLE DELETED: ";
     for (auto i = my_list.begin(); i != 0; i = i->next){
         cout << i->element << ' ';
     }
