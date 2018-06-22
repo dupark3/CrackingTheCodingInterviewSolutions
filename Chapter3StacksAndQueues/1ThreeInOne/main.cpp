29f64f03649a38a0e9c0d9ac0a9a538643459f28
DuParkLinux <dupark3@gmail.com>
2018-06-22 11:54:14 -0400

    Using three indexes to push/pop from the array that is divided into three sections.

--- Chapter3StacksAndQueues/1ThreeInOne/main.cpp
+++ Chapter3StacksAndQueues/1ThreeInOne/main.cpp
@@ -59,7 +59,7 @@ class Stack {
         }
 
         bool isEmpty(){
-            return top;
+            return !top;
         }
 
     private:
@@ -70,21 +70,99 @@ class Stack {
 
 /***************** SOLUTION TO THE PROBLEM ***********************/
 
-
-int main(){
-    Stack<int> my_stack;
-    if (my_stack.isEmpty()){
-        cout << "Empty stack\n";
+class ThreeStacks{
+public:
+    ThreeStacks() : array_size(15), first_stack_top(0), first_stack_begin(0),
+                    second_stack_top(15/3), second_stack_begin(15/3),
+                    third_stack_top(15/3 * 2), third_stack_begin (15/3 * 2) { 
     }
 
-    my_stack.push(5);
-    my_stack.push(2);
-    my_stack.push(7);
-    my_stack.push(10);
-    my_stack.push(9);
+    void push_first_stack(int val){
+        if (first_stack_top < second_stack_begin){
+            int_array[first_stack_top++] = val;
+        } else {
+            cout << "ERROR: FIRST STACK FULL, CAN'T PUSH " << val << ".\n";
+        }
+    }
 
-    cout << my_stack.pop() << endl;
-    cout << my_stack.pop() << endl;
+    void push_second_stack(int val){
+        if (second_stack_top < third_stack_begin){
+            int_array[second_stack_top++] = val;
+        } else {
+            cout << "ERROR: SECOND STACK FULL, CAN'T PUSH " << val << ".\n";
+        }
+    }
+
+    void push_third_stack(int val){
+        if (third_stack_top < array_size){
+            int_array[third_stack_top++] = val;
+        } else {
+            cout << "ERROR: THIRD STACK FULL, CAN'T PUSH " << val << ".\n";
+        }
+    }
+
+    int pop_first_stack(){
+        if (first_stack_top != first_stack_begin){
+            return int_array[--first_stack_top];
+        } else {
+            cout << "ERROR: FIRST STACK EMPTY" << endl;
+            return int();
+        }
+    }
+
+
+    int pop_second_stack(){
+        if (second_stack_top != third_stack_begin){
+            return int_array[--second_stack_top];
+        } else {
+            cout << "ERROR: SECOND STACK EMPTY" << endl;
+            return int();
+        }
+    }
+
+
+    int pop_third_stack(){
+        if (third_stack_top != array_size){
+            return int_array[--third_stack_top];
+        } else {
+            cout << "ERROR: THIRD STACK EMPTY" << endl;
+            return int();
+        }
+    }
+
+private:
+    const int array_size = 15;
+    int int_array[15];
+
+    int first_stack_top;
+    int second_stack_top;
+    int third_stack_top;
+
+    const int first_stack_begin;
+    const int second_stack_begin;
+    const int third_stack_begin;
+};
+
+int main(){
+    ThreeStacks my_stacks;
+    cout << my_stacks.pop_first_stack() << endl;
+    my_stacks.push_first_stack(5);
+    my_stacks.push_first_stack(2);
+    my_stacks.push_first_stack(15);
+    my_stacks.push_first_stack(9);
+    my_stacks.push_first_stack(1);
+    my_stacks.push_first_stack(2);
+    cout << my_stacks.pop_first_stack() << endl;
+    cout << my_stacks.pop_first_stack() << endl;
+
+    my_stacks.push_second_stack(90);
+    cout << my_stacks.pop_second_stack() << endl;
+
+    my_stacks.push_third_stack(150);
+    my_stacks.push_third_stack(160);
+    my_stacks.push_third_stack(170);
+    cout << my_stacks.pop_third_stack() << endl;
+
 
     return 0;
 }
