b01d6b6aa492bdabead93ac8c5b9b4e9845df202
DuParkLinux <dupark3@gmail.com>
2018-06-22 14:45:19 -0400

    Second stack method. Possibly saves space by adding the new min to a stack and popping that min stack when that min is popped from the main stack.

--- Chapter3StacksAndQueues/2StackMin/main.cpp
+++ Chapter3StacksAndQueues/2StackMin/main.cpp
@@ -15,10 +15,10 @@ class Node {
 
     public: 
         Node() { }
-        Node(T val) : element(val), min_element(), down(0) { }
+        Node(T val) : element(val), down(0), min_when_at_top(0) { }
     
         T element;
-        T min_element;
+        bool min_when_at_top;
         Node<T>* down;
 };
 
@@ -31,10 +31,19 @@ public:
     
     T pop() {
         if (top){
+            // if we are about to pop the min, delete the top of the min_node stack
+            if (top->min_when_at_top){
+                Node<T>* new_min = min_node->down;
+                delete min_node;
+                min_node = new_min;
+            }
+
+            // delete the top of the stack and update the top pointer to the one below 
             T ret = top->element;
             Node<T>* new_top = top->down;
             delete top;
             top = new_top;
+
             return ret;
         } else {
             return T();
@@ -43,15 +52,24 @@ public:
 
     void push(const T& val) {
         if (top){
+            // add to the top of the stack
             Node<T>* new_top = new Node<T>(val);
             new_top->down = top;
             top = new_top;
 
-            top->min_element = val < top->down->min_element ? 
-                               val : top->down->min_element;
+            // update min_node if added value is a new min
+            if (val < min_node->element){
+                top->min_when_at_top = true;
+                Node<T>* new_min = new Node<T>(val);
+                new_min->down = min_node;
+                min_node = new_min;
+            }
+
         } else {
+            // adding to an empty stack
             top = new Node<T>(val);
-            top->min_element = val;
+            min_node = new Node<T>(val);
+            min_node->min_when_at_top = true;
             top->down = 0;
         }
     }
@@ -69,8 +87,8 @@ public:
     }
 
     int min(){
-        if (top){
-            return top->min_element;
+        if (min_node){
+            return min_node->element;
         }
     }
 
@@ -85,6 +103,7 @@ public:
 
 private:
     Node<T>* top;
+    Node<T>* min_node;
 };
 
 
@@ -94,17 +113,18 @@ private:
 
 int main(){
     Stack<int> my_stack;
+
     if (my_stack.isEmpty()){
         cout << "Empty stack\n";
     }
     
-    my_stack.push(5);
+    my_stack.push(1);
     my_stack.push(4);
     my_stack.push(2);
-    my_stack.push(7);
+    my_stack.push(-7);
     my_stack.push(10);
     my_stack.push(15);
-    my_stack.push(1);
+    my_stack.push(0);
 
     my_stack.print();
 
