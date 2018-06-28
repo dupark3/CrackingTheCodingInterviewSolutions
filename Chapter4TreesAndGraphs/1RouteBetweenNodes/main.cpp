cec2489ba27d910109a2a7e35445b3359ec3cd10
DuParkLinux <dupark3@gmail.com>
2018-06-28 08:14:38 -0400

    Changed Graph class to have a vector of Node pointers instead of Node objects. BFS works to print each layer

--- Chapter4TreesAndGraphs/1RouteBetweenNodes/main.cpp
+++ Chapter4TreesAndGraphs/1RouteBetweenNodes/main.cpp
@@ -18,37 +18,30 @@ class Graph{
 public:
     Graph() : nodes() { }
     Graph(int n){
-        Node new_node(n);
+        Node* new_node = new Node(n);
         nodes.push_back(new_node);
     }
 
     void link_new_node(int new_value, int parent){
         for (int i = 0; i != nodes.size(); ++i){
-            
-            if (nodes[i].element == parent){
-                cout << "found parent " << nodes[i].element << " at " << i << endl; 
+            if (nodes[i]->element == parent){
                 Node* new_node = new Node(new_value);
-                nodes.push_back(*new_node);
-                nodes[i].children.push_back(new_node);
+                nodes.push_back(new_node);
+                nodes[i]->children.push_back(new_node);
             }
         }
     }
 
     void BFS(int root){
-        cout << "nodes size " << nodes.size() << endl;
         for (int i = 0; i != nodes.size(); ++i){
-            if (nodes[i].element == root){
-                queue<Node> my_queue;
+            if (nodes[i]->element == root){
+                queue<Node*> my_queue;
                 my_queue.push(nodes[i]);
                 while (!my_queue.empty()){
-                    cout << my_queue.front().element << " " << endl;
-                    cout << "children size " << my_queue.front().children.size() << endl;
-                    for (int j = 0; j != my_queue.front().children.size(); ++j){
-                        cout << "children size " << my_queue.front().children.size() << endl;
-                        cout << "pushing " << my_queue.front().children[j]->element << endl;
-                        my_queue.push(*my_queue.front().children[j]);
+                    cout << my_queue.front()->element << " ";
+                    for (int j = 0; j != my_queue.front()->children.size(); ++j){
+                        my_queue.push(my_queue.front()->children[j]);
                     }
-                    cout << "popping " << my_queue.front().element << endl;
                     my_queue.pop();
                 }
                 cout << endl;
@@ -57,7 +50,7 @@ public:
         }
     }
 private:
-    vector<Node> nodes;
+    vector<Node*> nodes;
 };
 
 
@@ -69,6 +62,8 @@ int main(){
     my_graph.link_new_node(3, 5);
     my_graph.link_new_node(4, 5);
     my_graph.link_new_node(0, 4);
+    my_graph.link_new_node(9, 4);
+    my_graph.link_new_node(8, 4);
     my_graph.link_new_node(2, 3);
     my_graph.link_new_node(1, 2);
 
